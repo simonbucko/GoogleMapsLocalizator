3ef2cfd2ac3c4cf6884fda1139b6264759f188aa
simonbucko <38184399+simonbucko@users.noreply.github.com>
2020-05-17 15:53:10 +0200

    ahoj

--- js/index.js
+++ js/index.js
@@ -1,147 +1,170 @@
 
-window.onload = function() {
+window.onload = function () {
+  closeButton = document.querySelector('.closeButton');
+  warningContent = document.querySelector('.warning');
+
+  closeButton.addEventListener('click', () => {
+    warningContent.style.display = 'none';
+  });
+  window.addEventListener('click', (e) => {
+    if (e.target == warningContent) warningContent.style.display = 'none';
+  })
 }
+
 
 var map;
 var markers = [];
 var infoWindow;
+let closeButton;
+let warningContent;
+let counter = 0;
 
 function initMap() {
-    var losAngeles = {
-        lat: 34.063380, 
-        lng: -118.358080
-    };
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: losAngeles,
-        zoom: 11,
-        mapTypeId: 'roadmap',
-        styles: [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-            {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-            }
-          ]
-    });
-    infoWindow = new google.maps.InfoWindow();
-    searchStores();
+  var losAngeles = {
+    lat: 34.063380,
+    lng: -118.358080
+  };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: losAngeles,
+    zoom: 11,
+    mapTypeId: 'roadmap',
+    styles: [
+      { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+      { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+      { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{ color: '#263c3f' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#6b9a76' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{ color: '#38414e' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#212a37' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#9ca5b3' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{ color: '#746855' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#1f2835' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#f3d19c' }]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{ color: '#2f3948' }]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{ color: '#17263c' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#515c6d' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: '#17263c' }]
+      }
+    ]
+  });
+  infoWindow = new google.maps.InfoWindow();
+  searchStores();
 }
 
-function searchStores(){
-    var foundStores = [];
-    var zipCode = document.getElementById('zip-code-input').value;
-    if(zipCode){
-        for(var store of stores){
-            var postal = store['address']['postalCode'].substring(0, 5);
-            if(postal == zipCode){
-                foundStores.push(store);
-            }
-        }
-    } else {
-        foundStores = stores;
+function searchStores() {
+  var foundStores = [];
+  var zipCode = document.getElementById('zip-code-input').value;
+  let zipCodeNumber;
+  if (zipCode) zipCodeNumber = parseInt(zipCode.match(/\d/g).join(''), 10);
+  if (zipCode.length == 5 && zipCodeNumber >= 10000 && zipCodeNumber < 100000) {
+    for (var store of stores) {
+      var postal = store['address']['postalCode'].substring(0, 5);
+      if (postal == zipCode) {
+        foundStores.push(store);
+      }
     }
-    clearLocations();
-    displayStores(foundStores);
-    showStoresMarkers(foundStores);
-    setOnClickListener();
-}
-
-function clearLocations(){
-    infoWindow.close();
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(null);
+    if (foundStores.length == 0) {
+      foundStores = stores;
+      document.querySelector('.warning').style.display = 'flex';
     }
-    markers.length = 0;
+  } else {
+    if (counter > 0) document.querySelector('.warning').style.display = 'flex';
+    counter++;
+    foundStores = stores;
+    let storesPostalCode = stores.map(store => store['address']['postalCode'].substring(0, 5));
+    let uniqPostalCodes = [...new Set(storesPostalCode.sort(function (a, b) { return a - b }))];
+  }
+  clearLocations();
+  displayStores(foundStores);
+  showStoresMarkers(foundStores);
+  setOnClickListener();
 }
 
-function setOnClickListener(){
-    var storeElements = document.querySelectorAll('.store-container');
-    storeElements.forEach(function(elem, index){
-        elem.addEventListener('click', function(){
-            new google.maps.event.trigger(markers[index], 'click');
-        })
+function clearLocations() {
+  infoWindow.close();
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers.length = 0;
+}
+
+function setOnClickListener() {
+  var storeElements = document.querySelectorAll('.store-container');
+  storeElements.forEach(function (elem, index) {
+    elem.addEventListener('click', function () {
+      new google.maps.event.trigger(markers[index], 'click');
     })
+  })
 }
 
-function displayStores(stores){
-    var storesHtml = '';
-    for(var [index, store] of stores.entries()){
-        var address = store['addressLines'];
-        var phone = store['phoneNumber'];
-        storesHtml += `
+function displayStores(stores) {
+  var storesHtml = '';
+  for (var [index, store] of stores.entries()) {
+    var address = store['addressLines'];
+    var phone = store['phoneNumber'];
+    storesHtml += `
             <div class="store-container">
                 <div class="store-container-background">
                     <div class="store-info-container">
@@ -153,35 +176,35 @@ function displayStores(stores){
                     </div>
                     <div class="store-number-container">
                         <div class="store-number">
-                            ${index+1}
+                            ${index + 1}
                         </div>
                     </div>
                 </div>
             </div>
         `
-        document.querySelector('.stores-list').innerHTML = storesHtml;
-    }
+    document.querySelector('.stores-list').innerHTML = storesHtml;
+  }
 }
 
 
-function showStoresMarkers(stores){
-    var bounds = new google.maps.LatLngBounds();
-    for(var [index, store] of stores.entries()){
-        var latlng = new google.maps.LatLng(
-            store["coordinates"]["latitude"],
-            store["coordinates"]["longitude"]);
-        var name = store["name"];
-        var address = store["addressLines"][0];
-        var openStatusText = store["openStatusText"]
-        var phoneNumber = store["phoneNumber"];
-        bounds.extend(latlng);
-        createMarker(latlng, name, address, openStatusText, phoneNumber, index+1);
-    }
-    map.fitBounds(bounds);
+function showStoresMarkers(stores) {
+  var bounds = new google.maps.LatLngBounds();
+  for (var [index, store] of stores.entries()) {
+    var latlng = new google.maps.LatLng(
+      store["coordinates"]["latitude"],
+      store["coordinates"]["longitude"]);
+    var name = store["name"];
+    var address = store["addressLines"][0];
+    var openStatusText = store["openStatusText"]
+    var phoneNumber = store["phoneNumber"];
+    bounds.extend(latlng);
+    createMarker(latlng, name, address, openStatusText, phoneNumber, index + 1);
+  }
+  map.fitBounds(bounds);
 }
 
-function createMarker(latlng, name, address, openStatusText, phoneNumber, index){
-    var html = `
+function createMarker(latlng, name, address, openStatusText, phoneNumber, index) {
+  var html = `
         <div class="store-info-window">
             <div class="store-info-name">
                 ${name}
@@ -203,14 +226,14 @@ function createMarker(latlng, name, address, openStatusText, phoneNumber, index)
             </div>
         </div>
     `;
-    var marker = new google.maps.Marker({
-      map: map,
-      position: latlng,
-      label: index.toString()
-    });
-    google.maps.event.addListener(marker, 'click', function() {
-      infoWindow.setContent(html);
-      infoWindow.open(map, marker);
-    });
-    markers.push(marker);
+  var marker = new google.maps.Marker({
+    map: map,
+    position: latlng,
+    label: index.toString()
+  });
+  google.maps.event.addListener(marker, 'click', function () {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+  markers.push(marker);
 }
